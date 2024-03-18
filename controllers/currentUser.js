80e0cac949e2743d97a27329ce6ca3705107105f
Mart <marta.nikiel@yahoo.com>
2024-03-18 23:30:53 +0100

    sprawdzenie rejestracji

--- controllers/currentUser.js
+++ controllers/currentUser.js
@@ -1,15 +1,15 @@
 const currentUser = async (req, res, next) => {
   try {
-    const currentUser = req.user;
-    if (!currentUser) {
+    const { name, email } = req.user;
+    if (!name && !email) {
       return res.status(401).json({ message: "Not authorized" });
     }
     res.status(200).json({
-      email: currentUser.email,
+      name,
+      email,
     });
   } catch (error) {
     next(error);
   }
 };
-
 module.exports = { currentUser };
