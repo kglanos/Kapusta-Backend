79c83178ecb8c0528b797fc40ab6c234052100fb
Kamil Mrowka <kamil@chicos.life>
2024-03-26 08:54:58 +0100

    App.js można lepiej trochę organizować

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-const userRouter = require("./routes/userRouter");
-const transactionRouter = require("./routes/transactionRouter")
 require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const logger = require("morgan");
+
+const transactionRouter = require("./routes/transactionRouter")
+const userRouter = require("./routes/userRouter");
 
 const app = express();
 
@@ -12,11 +15,15 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use("/", userRouter);
+app.use("/", userRouter); // ten route jest ten sam do tego poniżej
 app.use("/", transactionRouter)
+/*
+app.use("/auth", userRouter);
+app.use("/transactions", transactionRouter)
+*/
 
 app.get("/", (req, res) => {
-  res.send("Server is working!");
+  res.send("Server is working!"); // this is not needed. it is used already in server.js
 });
 
 app.use((req, res) => {
