03c49848a60b411c90ed29a8e7232d998b11be2f
Kamil Mrowka <kamil@chicos.life>
2024-03-26 09:17:33 +0100

    Komentarz odnośnie tryCatchWrapper

--- utils/tryCatchWrapper.js
+++ utils/tryCatchWrapper.js
@@ -2,9 +2,10 @@ const tryCatchWrapper = (endpointFn) => {
   return async (req, res, next) => {
     try {
       await endpointFn(req, res, next);
+      // throw 'errr'; //wystarczy to odkomentować i zobaczyć jak ta aplikacja przestaje działać.
     } catch (error) {
       console.log(error);
-      throw error;
+      throw error; // To będzie wam blokować aplikację. Bo po throw to musi zostać obsłużone wyżej przez inny blok catch
     }
   };
 };
